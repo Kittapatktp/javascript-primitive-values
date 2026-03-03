da1fe869db3ee1861c23e7fdb90bca4f01580b21
Your NamePEem <you@example.comkittapat.ktp@gmail.com>
2026-03-03 23:45:37 +0700

    Homework(exercise) : Do excerxise 1-8

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,4 @@
 
 let totalFileSize = 250000;
 let uploadedSize = 30000;
-
-// Start coding here
+let uploadProgress  = (uploadedSize / totalFileSize)* 100// Start coding here
--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000 
+let Discount = 0.25 * totalPrice  
+let amountPrice = totalPrice - Discount
+console.log(amountPrice)
+
